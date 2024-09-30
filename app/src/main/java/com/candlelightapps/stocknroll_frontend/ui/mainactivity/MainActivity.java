a57c616c6aa5e597f39567d27d314459030017b7
filip-byte <ohboytoby@gmail.com>
2024-09-30 12:01:32 +0100

    chore: Add MVVM project structure

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/mainactivity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.candlelightapps.stocknroll_frontend;
+package com.candlelightapps.stocknroll_frontend.ui.mainactivity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.candlelightapps.stocknroll_frontend.R;
 
 public class MainActivity extends AppCompatActivity {
 
