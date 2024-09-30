96f3d792e21ddc4cfc32cb39ff6f8e29166763af
filip-byte <ohboytoby@gmail.com>
2024-09-30 12:38:52 +0100

    fix: Remove duplicated layout
    - activity_favourite_recipes2.xml deleted
    
    - FavouriteRecipesActivity pointing to the correct layout -> activity_favourite_recipes.xml

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/favouriterecipes/FavouriteRecipesActivity.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/favouriterecipes/FavouriteRecipesActivity.java
@@ -16,7 +16,7 @@ public class FavouriteRecipesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_favourite_recipes2);
+        setContentView(R.layout.activity_favourite_recipes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
