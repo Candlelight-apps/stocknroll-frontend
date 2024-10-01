7cce75e3668a391b6bfce33f5cd32e54769082d3
Reece Moore <reecemoore@Andres-MacBook-Pro.local>
2024-10-01 12:21:51 +0200

    Feat: added the recipe data model

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/model/Recipe.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/model/Recipe.java
@@ -1,4 +1,68 @@
 package com.candlelightapps.stocknroll_frontend.model;
 
-public class Recipe {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class Recipe extends BaseObservable {
+
+     String title;
+     int readyInMinutes;
+     String sourceURL;
+     String image;
+     Boolean isFavourite;
+
+    public Recipe(String title, int readyInMinutes, String sourceURL, String image, Boolean isFavourite) {
+        this.title = title;
+        this.readyInMinutes = readyInMinutes;
+        this.sourceURL = sourceURL;
+        this.image = image;
+        this.isFavourite = isFavourite;
+    }
+
+    public Recipe() {}
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Bindable
+    public int getReadyInMinutes() {
+        return readyInMinutes;
+    }
+
+    public void setReadyInMinutes(int readyInMinutes) {
+        this.readyInMinutes = readyInMinutes;
+    }
+
+    @Bindable
+    public String getSourceURL() {
+        return sourceURL;
+    }
+
+    public void setSourceURL(String sourceURL) {
+        this.sourceURL = sourceURL;
+    }
+
+    @Bindable
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Bindable
+    public Boolean getFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(Boolean favourite) {
+        isFavourite = favourite;
+    }
 }
