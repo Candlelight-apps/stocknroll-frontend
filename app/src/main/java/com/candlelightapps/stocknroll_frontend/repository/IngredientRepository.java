f4d8bf431497523afd9806c4901b797aa3db1dae
Reece Moore <reecemoore@Andres-MacBook-Pro.local>
2024-10-01 14:18:00 +0200

    Feat: Retrofit instance created.

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/repository/IngredientRepository.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/repository/IngredientRepository.java
@@ -1,4 +1,20 @@
 package com.candlelightapps.stocknroll_frontend.repository;
 
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.candlelightapps.stocknroll_frontend.model.Ingredient;
+import com.candlelightapps.stocknroll_frontend.service.ApiService;
+import com.candlelightapps.stocknroll_frontend.service.RetrofitInstance;
+
 public class IngredientRepository {
+
+    private final MutableLiveData<Ingredient> ingredientsList;
+    private final ApiService apiService;
+
+    public IngredientRepository(Application application) {
+        ingredientsList = new MutableLiveData<>();
+        apiService = RetrofitInstance.
+    }
 }
