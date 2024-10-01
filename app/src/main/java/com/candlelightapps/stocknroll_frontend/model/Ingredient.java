eeb21ec40cd381896affa333298894b9b3b52e79
Reece Moore <reecemoore@Andres-MacBook-Pro.local>
2024-10-01 12:13:31 +0200

    Feat: Ingredient data model completed

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/model/Ingredient.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/model/Ingredient.java
@@ -1,4 +1,72 @@
 package com.candlelightapps.stocknroll_frontend.model;
 
-public class Ingredient {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.candlelightapps.stocknroll_frontend.BR;
+
+import java.time.LocalDate;
+
+public class Ingredient extends BaseObservable {
+
+    String name;
+    String category;
+    int quantity;
+    LocalDate expiryDate;
+    String imageUrl; //To be implemented later
+
+    public Ingredient(String name, String category, int quantity, LocalDate expiryDate, String imageUrl) {
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.expiryDate = expiryDate;
+        this.imageUrl = imageUrl;
+    }
+
+    public Ingredient() {}
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Bindable
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Bindable
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Bindable
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Bindable
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
