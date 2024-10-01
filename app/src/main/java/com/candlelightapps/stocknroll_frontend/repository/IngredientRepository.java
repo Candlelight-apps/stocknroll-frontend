bb4bb6b950abaa41b89a285ca5bc787ac5f6d269
Reece Moore <reecemoore@Andres-MacBook-Pro.local>
2024-10-01 14:21:53 +0200

    Feat: Ingredients repository created.

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/repository/IngredientRepository.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/repository/IngredientRepository.java
@@ -8,13 +8,37 @@ import com.candlelightapps.stocknroll_frontend.model.Ingredient;
 import com.candlelightapps.stocknroll_frontend.service.ApiService;
 import com.candlelightapps.stocknroll_frontend.service.RetrofitInstance;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class IngredientRepository {
 
-    private final MutableLiveData<Ingredient> ingredientsList;
+    private final MutableLiveData<List<Ingredient>> ingredientsList;
     private final ApiService apiService;
 
     public IngredientRepository(Application application) {
         ingredientsList = new MutableLiveData<>();
-        apiService = RetrofitInstance.
+        apiService = RetrofitInstance.getRetrofitInstance().create(ApiService.class);
+    }
+
+    public MutableLiveData<List<Ingredient>> getMutableLiveData() {
+        apiService.getAllIngredients().enqueue(new Callback<List<Ingredient>>() {
+            @Override
+            public void onResponse(Call<List<Ingredient>> call, Response<List<Ingredient>> response) {
+                if(response.isSuccessful() && response.body() != null) {
+                    ingredientsList.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Ingredient>> call, Throwable t) {
+                ingredientsList.setValue(null);
+
+            }
+        });
+        return ingredientsList;
     }
 }
