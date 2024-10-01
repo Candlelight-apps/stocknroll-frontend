cddd24b570847ad85971a96fd626dd03dae29c1c
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-01 11:53:55 +0100

    refactor: change id to quantity for item recyclerview

---
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/IngredientAdapter.java
@@ -0,0 +1,10 @@
+package com.candlelightapps.stocknroll_frontend.ui.findrecipebyingredient;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends RecyclerView.Adapter<>{
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        
+    }
+}
