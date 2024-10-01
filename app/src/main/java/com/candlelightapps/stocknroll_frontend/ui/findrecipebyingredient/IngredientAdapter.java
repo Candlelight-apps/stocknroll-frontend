1adc32d61fcf09d3bc27f46135d6c7316a59a754
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-01 11:55:23 +0100

    feat: IngredientViewHolder methods implemented

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/IngredientAdapter.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/IngredientAdapter.java
@@ -1,10 +1,24 @@
 package com.candlelightapps.stocknroll_frontend.ui.findrecipebyingredient;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ItemAdapter extends RecyclerView.Adapter<>{
+import com.candlelightapps.stocknroll_frontend.R;
 
-    public static class ItemViewHolder extends RecyclerView.ViewHolder {
-        
+public class IngredientAdapter extends RecyclerView.Adapter<>{
+
+    public static class IngredientViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView itemName;
+        public TextView quantity;
+
+        public IngredientViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemName = (TextView) itemView.findViewById(R.id.nameOfItem);
+            quantity = (TextView) itemView.findViewById(R.id.quantity);
+        }
     }
 }
