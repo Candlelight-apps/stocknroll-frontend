f7e3fe2a7dafa16f48973933aad913aac6642f86
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-01 15:02:41 +0100

    feat: onCreateViewHolder  method for RecipeAdapter implemented

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/RecipeAdapter.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/RecipeAdapter.java
@@ -1,13 +1,52 @@
 package com.candlelightapps.stocknroll_frontend.ui.findrecipebyingredient;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.candlelightapps.stocknroll_frontend.R;
 import com.candlelightapps.stocknroll_frontend.databinding.ActivityFoundRecipeViewBinding;
+import com.candlelightapps.stocknroll_frontend.model.Recipe;
 
-public class RecipeAdapter extends RecyclerView.Adapter<> {
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    List<Recipe> recipeList;
+    Context context;
+
+    public RecipeAdapter(List<Recipe> recipeList, Context context) {
+        this.recipeList = recipeList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ActivityFoundRecipeViewBinding activityFoundRecipeViewBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.activity_found_recipe_view,
+                parent,
+                false
+        );
+
+        return new RecipeViewHolder(activityFoundRecipeViewBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public static class RecipeViewHolder extends RecyclerView.ViewHolder {
 
