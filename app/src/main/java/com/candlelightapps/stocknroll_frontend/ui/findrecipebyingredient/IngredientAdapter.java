d6fa3ac8e81d1f4a6c13bd3630d085571c5dfc23
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-01 14:36:49 +0100

    feat: onCreateViewHolder implemented for IngredientAdapter

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/IngredientAdapter.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/IngredientAdapter.java
@@ -1,11 +1,13 @@
 package com.candlelightapps.stocknroll_frontend.ui.findrecipebyingredient;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.candlelightapps.stocknroll_frontend.R;
@@ -16,7 +18,40 @@ import java.util.List;
 
 public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
 
+    List<Ingredient> ingredientList;
+    Context context;
+
+    public IngredientAdapter(List<Ingredient> ingredientList, Context context) {
+        this.ingredientList = ingredientList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ActivityFindRecipeByIngredientBinding activityFindRecipeByIngredientBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.activity_find_recipe_by_ingredient,
+                parent,
+                false
+        );
+
+        return new IngredientViewHolder(activityFindRecipeByIngredientBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public static class IngredientViewHolder extends RecyclerView.ViewHolder {
+
         private ActivityFindRecipeByIngredientBinding activityFindRecipeByIngredientBinding;
 
         public IngredientViewHolder(ActivityFindRecipeByIngredientBinding activityFindRecipeByIngredientBinding) {
