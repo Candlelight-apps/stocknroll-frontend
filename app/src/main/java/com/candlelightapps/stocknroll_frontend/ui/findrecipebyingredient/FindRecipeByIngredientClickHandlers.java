f92d22f4d6c3d2a910ce270e4ce78a6f136c0ee9
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-10-02 14:54:18 +0100

    alertdialog for onBackBtnClicked()

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/FindRecipeByIngredientClickHandlers.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/FindRecipeByIngredientClickHandlers.java
@@ -1,6 +1,8 @@
 package com.candlelightapps.stocknroll_frontend.ui.findrecipebyingredient;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 
@@ -11,7 +13,17 @@ public class FindRecipeByIngredientClickHandlers {
     Context context;
 
     public void onBackBtnClicked(View view) {
-        Intent intent = new Intent(view.getContext(), MainActivity.class);
-        context.startActivity(intent);
+        new AlertDialog.Builder(context)
+                .setTitle("Back to Home")
+                .setMessage("Are you sure you want to go Home? All progress will be lost.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                        context.startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
