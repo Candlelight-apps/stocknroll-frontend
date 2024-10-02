ede53a46992cfc3739e8ce4d8be8486a2492bd07
rsheikh <red.sheikh@gmail.com>
2024-10-02 22:08:08 +0100

    feat: Create IngredientViewModel to encompass ingredient-related repository access

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/viewmodel/IngredientViewModel.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/viewmodel/IngredientViewModel.java
@@ -1,14 +1,22 @@
-package com.candlelightapps.stocknroll_frontend.ui.mainactivity;
+package com.candlelightapps.stocknroll_frontend.ui.viewmodel;
 
 import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
-public class MainActivityViewModel extends AndroidViewModel {
+import com.candlelightapps.stocknroll_frontend.model.Ingredient;
+import com.candlelightapps.stocknroll_frontend.repository.IngredientRepository;
 
+public class IngredientViewModel extends AndroidViewModel {
 
-    public MainActivityViewModel(@NonNull Application application) {
+    IngredientRepository ingredientRepository;
+
+    public IngredientViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredientRepository.addIngredient(ingredient);
     }
 }
