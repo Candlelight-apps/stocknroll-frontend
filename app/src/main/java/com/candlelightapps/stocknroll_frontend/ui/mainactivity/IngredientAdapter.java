a98dc7dec6a5253d5c9ca9b818efe0248bcc7a6c
filip-byte <ohboytoby@gmail.com>
2024-10-02 12:56:57 +0100

    feat: Implement RecyclerView for inventory
    - item_layout.xml added
    
    - Data binding implemented

---
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/mainactivity/IngredientAdapter.java
@@ -0,0 +1,60 @@
+package com.candlelightapps.stocknroll_frontend.ui.mainactivity;
+
+import android.content.Context;
+import android.media.RouteListingPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.candlelightapps.stocknroll_frontend.R;
+import com.candlelightapps.stocknroll_frontend.databinding.ItemIngredientViewBinding;
+import com.candlelightapps.stocknroll_frontend.model.Ingredient;
+
+import java.util.List;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+
+    private Context context;
+    private List<Ingredient> ingredientList;
+
+    public IngredientAdapter(Context context, List<Ingredient> ingredientList) {
+        this.context = context;
+        this.ingredientList = ingredientList;
+    }
+
+    @NonNull
+    @Override
+    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemIngredientViewBinding itemIngredientViewBinding = ItemIngredientViewBinding.inflate(LayoutInflater.from(
+                parent.getContext()), parent, false);
+        return new IngredientViewHolder(itemIngredientViewBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
+        Ingredient ingredient = ingredientList.get(position);
+        holder.nameTextView.setText(ingredient.getName());
+        holder.categoryTextView.setText(ingredient.getCategory());
+        holder.quantityTextView.setText(String.valueOf(ingredient.getQuantity()));
+        holder.expiryDateTextView.setText(ingredient.getExpiryDate().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredientList.size();
+    }
+
+    public static class IngredientViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemIngredientViewBinding itemIngredientViewBinding;
+
+        public IngredientViewHolder(ItemIngredientViewBinding itemIngredientViewBinding) {
+            super(itemIngredientViewBinding.getRoot());
+            this.itemIngredientViewBinding = itemIngredientViewBinding;
+        }
+    }
+}
