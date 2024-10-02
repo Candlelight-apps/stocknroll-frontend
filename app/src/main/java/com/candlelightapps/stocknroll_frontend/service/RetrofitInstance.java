3aead821d97d65c08d9df4b2f5b76fd7630b004c
rsheikh <red.sheikh@gmail.com>
2024-10-02 22:15:18 +0100

    feat: Modify class to create a separate retrofit instance for the new IngredientApiService class

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/service/RetrofitInstance.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/service/RetrofitInstance.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "/api/v1/stocknroll/";
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/stocknroll/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
@@ -27,4 +27,23 @@ public class RetrofitInstance {
         }
         return retrofit;
     }
+
+
+    public static IngredientApiService getIngredientApiService() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+
+        return retrofit.create(IngredientApiService.class);
+}
+
 }
