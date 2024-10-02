caf9e9c230e1b0ec98218bf246789fc3b25a772d
Reece Moore <reecemoore@Andres-MacBook-Pro.local>
2024-10-02 10:59:27 +0100

    Feat: base URL updated

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/service/RetrofitInstance.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/service/RetrofitInstance.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = ""; //Todo - update with the base URL
+    private static final String BASE_URL = "/api/v1/stocknroll/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
