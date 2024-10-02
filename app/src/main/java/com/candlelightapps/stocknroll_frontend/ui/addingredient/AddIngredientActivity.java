55f92dd8ded88ab02e45e20ade3d9ab2b51dfa1f
rsheikh <red.sheikh@gmail.com>
2024-10-02 22:02:02 +0100

    feat: Create AddIngredientActivity class

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/addingredient/AddIngredientActivity.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/addingredient/AddIngredientActivity.java
@@ -2,25 +2,34 @@ package com.candlelightapps.stocknroll_frontend.ui.addingredient;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.candlelightapps.stocknroll_frontend.R;
+import com.candlelightapps.stocknroll_frontend.model.Ingredient;
+import com.candlelightapps.stocknroll_frontend.ui.mainactivity.MainActivity;
+import com.candlelightapps.stocknroll_frontend.ui.mainactivity.MainActivityClickHandler;
+import com.candlelightapps.stocknroll_frontend.ui.viewmodel.IngredientViewModel;
 
 public class AddIngredientActivity extends AppCompatActivity {
+
+//    private ActivityAddIngredientBinding binding;
+    private AddIngredientClickHandler ingredientClickHandler;
+    private Ingredient ingredient;
+    IngredientViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_ingredient);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ingredient = new Ingredient();
+
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_ingredient);
+
+        viewModel = new ViewModelProvider(this).get(IngredientViewModel.class);
+        ingredientClickHandler = new AddIngredientClickHandler(ingredient, this, viewModel);
+//        binding.setIngredient(ingredient);
     }
 }
