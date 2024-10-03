0a01afdded6a43c80423375ded8bb74563ca8504
rsheikh <red.sheikh@gmail.com>
2024-10-03 12:22:06 +0100

    feat: Add endpoints for ingredients to the ingredientAPIService

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/service/IngredientApiService.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/service/IngredientApiService.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface IngredientApiService {
 
@@ -17,4 +20,9 @@ public interface IngredientApiService {
     @POST("ingredients")
     Call<Ingredient> addIngredient(@Body Ingredient ingredient);
 
+    @DELETE("ingredients/{id}")
+    Call<Ingredient> deleteIngredient(@Path("id") long id);
+
+    @PATCH
+    Call<Ingredient> updateIngredient(@Path("id") long id, @Body int quantity);
 }
