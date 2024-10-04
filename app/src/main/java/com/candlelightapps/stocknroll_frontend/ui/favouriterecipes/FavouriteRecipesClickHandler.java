38be7d2e468656a9ea82e4dfde0ef9342166d178
rsheikh <red.sheikh@gmail.com>
2024-10-04 18:32:17 +0100

    feat: implement getRecipes endpoint method

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/favouriterecipes/FavouriteRecipesClickHandler.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/favouriterecipes/FavouriteRecipesClickHandler.java
@@ -1,2 +1,22 @@
-package com.candlelightapps.stocknroll_frontend.ui.favouriterecipes;public class RecipeViewClickHandler {
+package com.candlelightapps.stocknroll_frontend.ui.favouriterecipes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.candlelightapps.stocknroll_frontend.ui.addingredient.AddIngredientActivity;
+
+public class FavouriteRecipesClickHandler {
+
+    Context context;
+
+    public FavouriteRecipesClickHandler(Context context) {
+        this.context = context;
+    }
+
+    public void onFABClicked(View view) {
+        Intent intent = new Intent(view.getContext(), FavouriteRecipesActivity.class);
+
+        context.startActivity(intent);
+    }
 }
