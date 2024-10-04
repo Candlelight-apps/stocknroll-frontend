3d80ff1784dd7c5998056bef18ec7f37d19a8c3a
Liam O'Hara <liam.ohara@gmail.com>
2024-10-04 15:47:26 +0100

    Feat: implement getRecipesByIngredients and displayInRecyclerView methods in FoundRecipeByIngredient.

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/FoundRecipeByIngredient.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/findrecipebyingredient/FoundRecipeByIngredient.java
@@ -7,20 +7,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.candlelightapps.stocknroll_frontend.R;
+import com.candlelightapps.stocknroll_frontend.databinding.ActivityFoundRecipeByIngredientBinding;
+import com.candlelightapps.stocknroll_frontend.model.Recipe;
+import com.candlelightapps.stocknroll_frontend.ui.viewmodel.RecipeViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoundRecipeByIngredient extends AppCompatActivity {
+
+    private List<String> ingredientList;
+    private List<Recipe> recipeList;
+    private ActivityFoundRecipeByIngredientBinding foundRecipeByIngredientBinding;
+    private RecipeViewModel viewModel;
+    private RecyclerView recyclerView;
+    private RecipeAdapter recipeAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_found_recipe_by_ingredient);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        foundRecipeByIngredientBinding = DataBindingUtil.setContentView(this, R.layout.activity_found_recipe_by_ingredient);
+
+        viewModel = new ViewModelProvider(this)
+                .get(RecipeViewModel.class);
+
+    }
+
+    private void getRecipesByIngredients(List<String> ingredientList) {
+        viewModel.getRecipesByIngredients(ingredientList).observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(List<Recipe> recipes) {
+                recipeList = (ArrayList<Recipe>) recipes;
+                displayInRecyclerView();
+            }
         });
+    }
+
+    public void displayInRecyclerView() {
+        recyclerView = foundRecipeByIngredientBinding.recipeRecyclerView;
+        recipeAdapter = new RecipeAdapter(recipeList, this);
+        recyclerView.setAdapter(recipeAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recipeAdapter.notifyDataSetChanged();
     }
 }
