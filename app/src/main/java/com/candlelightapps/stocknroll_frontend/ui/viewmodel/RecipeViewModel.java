a81077f3577353d8ba51ede6c7f4d8c3b6ad1cd7
Liam O'Hara <liam.ohara@gmail.com>
2024-10-06 20:12:04 +0100

    Feat: implement addRecipe method in RecipeViewModel.

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/ui/viewmodel/RecipeViewModel.java
@@ -28,4 +28,8 @@ public class RecipeViewModel extends AndroidViewModel {
         return recipeRepository.getRecipesByIngredients(ingredients);
     }
 
+    public void addRecipe(Recipe recipe) {
+        recipeRepository.addRecipe(recipe);
+    }
+
 }
