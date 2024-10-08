4d0ef839b884720b1d1b345b7fb63a92b89d7d59
princyjp <princy.philip@outlook.com>
2024-10-08 01:25:12 +0100

    refactor:add new method intToStringDisplay() in Converter to display 0

--- app/src/main/java/com/candlelightapps/stocknroll_frontend/util/Converter.java
+++ app/src/main/java/com/candlelightapps/stocknroll_frontend/util/Converter.java
@@ -7,13 +7,25 @@ import androidx.databinding.InverseMethod;
 public class Converter {
     @InverseMethod("stringToInt")
     public static String intToString(int value) {
-//        if (value == 0) {
-//            return "";
-//        }
+        if (value == 0) {
+            return "";
+        }
         return String.valueOf(value);
     }
 
     public static int stringToInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    @InverseMethod("stringToIntDisplay")
+    public static String intToStringDisplay(int value) {
+        return String.valueOf(value);
+    }
+
+    public static int stringToIntDisplay(String value) {
         try {
             return Integer.parseInt(value);
         } catch (NumberFormatException e) {
